0880afec8bb83cf0cc23064de1e507e90902088e
halle <halle.mills.2022@student.lhschools.org>
2019-09-27 09:40:41 -0400

    first commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,14 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int firstNumber = 87;
+        int secondNumber = 42;
+        if (80>40)
+            System.out.println(firstNumber + " is larger than "+secondNumber);
+
+
+    }
+}
