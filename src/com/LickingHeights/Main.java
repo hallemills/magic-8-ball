ebb5475b53dc5860966b378139ac73fb132abb02
halle <halle.mills.2022@student.lhschools.org>
2019-10-03 09:50:23 -0400

    fixed the int's

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,32 +5,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	//
+        //
         Scanner keyboard;
-        int firstNumber;
-        keyboard = new Scanner (System.in);
-        System.out.println("Pick a number 1-10");
-        firstNumber = keyboard.nextInt();
-        if(10>=firstNumber & firstNumber<0);
-         System.out.println("then");
-        {
+        keyboard = new Scanner(System.in);
+
+        Double number;
+
+        System.out.println("Pick a number 1-100");
+        number = keyboard.nextDouble();
+
+        if (10>=number && number>0) {
+            System.out.println("It is certain");
+        }
+        else if(20>=number && number>10) {
+            System.out.println("Without a doubt");
+        }
+        else if(30>=number && number>20) {
+            System.out.println("As I see it");
+        }
+        else if(40>=number && number>30) {
+            System.out.println("Its not lookin too hot");
+        }
+        else if(50>=number && number>40) {
+            System.out.println("Don't count on it");
+        }
+        else if(60>=number && number>50) {
+            System.out.println("Most likely");
+        
 
 
 
-        int secondNumber;
-        int thirdNumber;
-        int fourthNumber;
-        int fifthNumber;
-        int sixthNumber;
-        int seventhNumber;
-        int eigthnumber;
-        int ninthNumber;
-        int tenthNumber;
 
 
-            
+
 
         }
-
     }
-}
