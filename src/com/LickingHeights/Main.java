a60d2c9a0516ca67914644c0c65bacba404af77d
halle <halle.mills.2022@student.lhschools.org>
2019-10-03 09:17:01 -0400

    changed the sequence

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,13 +1,35 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int firstNumber = 87;
-        int secondNumber = 42;
-        if (80>40) {
-            System.out.println(firstNumber + " is larger than " + secondNumber);
+	//
+        Scanner keyboard;
+        int firstNumber;
+        keyboard = new Scanner (System.in);
+        System.out.println("Pick a number 1-10");
+        firstNumber = keyboard.nextInt();
+        if(10>=firstNumber & firstNumber<0);
+         System.out.println("then");
+        {
+
+
+
+        int secondNumber;
+        int thirdNumber;
+        int fourthNumber;
+        int fifthNumber;
+        int sixthNumber;
+        int seventhNumber;
+        int eigthnumber;
+        int ninthNumber;
+        int tenthNumber;
+
+
+            
+
         }
 
     }
